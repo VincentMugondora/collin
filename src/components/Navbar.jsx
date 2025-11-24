8ab49d0dd477b0aa1fae2e41f37bf55626e305ad
vincentmugondora <vinmugondora@gmail.com>
2025-11-24 09:47:00 +0200

    updating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-evenly'>
+    <div className='flex justify-between px-[60px] py-1.5'>
         <div>
             <img src="/logo.jpg" alt="" />
         </div>
-        <div>
+        <div className='flex'>
             <h1>Home</h1>
             <h1>About</h1>
             <h1>Projects</h1>
