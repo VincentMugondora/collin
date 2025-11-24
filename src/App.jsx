0e028a9ad298caf03cd9ac1e1ecaa96a3172878c
vincentmugondora <vinmugondora@gmail.com>
2025-11-24 15:14:06 +0200

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import './components/fonts.css'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import MyTripNatureTravel from './components/MyTripNatureTravel'
 
 const App = () => {
-  return (
-    <div className='bg-black text-white ' style={{ fontFamily: 'TerminaTest, system-ui, sans-serif', }}>
-    <Navbar />
-    <Hero />
-    <About />
-    </div>
-  )
+	return (
+		<div
+			className="bg-black text-white min-h-screen"
+			style={{ fontFamily: 'TerminaTest, system-ui, sans-serif' }}
+		>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={(
+						<>
+							<Hero />
+							<About />
+						</>
+					)}
+				/>
+				<Route path="/mytrip" element={<MyTripNatureTravel />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
