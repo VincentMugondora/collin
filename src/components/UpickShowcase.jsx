1ad889c7164be0f43384161da02e34726f987e64
vincentmugondora <vinmugondora@gmail.com>
2025-11-27 10:50:50 +0200

    updates

--- src/components/UpickShowcase.jsx
+++ src/components/UpickShowcase.jsx
@@ -5,7 +5,7 @@ const UpickShowcase = () => {
     <section className="bg-neutral-200 py-16 sm:py-20 md:py-24 px-4 sm:px-6 lg:px-10">
       <div className="max-w-7xl mx-auto">
         <div className="relative h-[560px] sm:h-[680px] md:h-[780px] lg:h-[880px]">
-          <div className="absolute -top-6 left-4 sm:left-8 rotate-[18deg]">
+          <div className="absolute -top-6 left-4 sm:left-8 rotate-18">
             <div className="relative aspect-9/19 w-40 sm:w-48 md:w-56 rounded-[2.5rem] bg-white shadow-[0_30px_80px_rgba(0,0,0,0.3)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-yellow-400 flex items-center justify-center">
                 <div className="w-14 h-14 rounded-xl bg-white/90" />
@@ -14,7 +14,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute top-10 right-2 sm:right-10 rotate-[-18deg]">
+          <div className="absolute top-10 right-2 sm:right-10 -rotate-18">
             <div className="relative aspect-9/19 w-44 sm:w-52 md:w-60 rounded-[2.5rem] bg-white shadow-[0_30px_80px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-white">
                 <div className="h-16 bg-yellow-400" />
@@ -27,7 +27,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute top-28 left-1/4 rotate-[22deg]">
+          <div className="absolute top-28 left-1/4 rotate-22">
             <div className="relative aspect-9/19 w-44 sm:w-52 md:w-60 rounded-[2.5rem] bg-white shadow-[0_35px_90px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-yellow-400 flex items-center justify-center">
                 <div className="w-16 h-16 rounded-xl bg-white/90" />
@@ -36,7 +36,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute top-32 right-1/4 rotate-[-20deg]">
+          <div className="absolute top-32 right-1/4 -rotate-20">
             <div className="relative aspect-9/19 w-40 sm:w-48 md:w-56 rounded-[2.5rem] bg-white shadow-[0_30px_80px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-white">
                 <div className="p-4 space-y-3">
@@ -50,7 +50,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute top-1/2 left-6 sm:left-12 rotate-[20deg]">
+          <div className="absolute top-1/2 left-6 sm:left-12 rotate-20">
             <div className="relative aspect-9/19 w-44 sm:w-52 md:w-60 rounded-[2.5rem] bg-white shadow-[0_40px_100px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-yellow-400 flex items-center justify-center">
                 <div className="w-14 h-14 rounded-xl bg-white/90" />
@@ -59,14 +59,14 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute top-[55%] right-12 rotate-[-22deg]">
+          <div className="absolute top-[55%] right-12 -rotate-22">
             <div className="relative aspect-9/19 w-44 sm:w-52 md:w-60 rounded-[2.5rem] bg-white shadow-[0_40px_100px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-yellow-400" />
               <div className="absolute top-3 left-1/2 -translate-x-1/2 w-16 h-3 rounded-full bg-neutral-300" />
             </div>
           </div>
 
-          <div className="absolute bottom-10 left-1/4 rotate-[18deg]">
+          <div className="absolute bottom-10 left-1/4 rotate-18">
             <div className="relative aspect-9/19 w-40 sm:w-48 md:w-56 rounded-[2.5rem] bg-white shadow-[0_30px_80px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-white">
                 <div className="h-16 bg-yellow-400" />
@@ -81,7 +81,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute bottom-6 right-1/3 rotate-[-18deg]">
+          <div className="absolute bottom-6 right-1/3 -rotate-18">
             <div className="relative aspect-9/19 w-40 sm:w-48 md:w-56 rounded-[2.5rem] bg-white shadow-[0_30px_80px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-white">
                 <div className="p-4 space-y-3">
@@ -95,7 +95,7 @@ const UpickShowcase = () => {
             </div>
           </div>
 
-          <div className="absolute -bottom-10 right-6 rotate-[-22deg]">
+          <div className="absolute -bottom-10 right-6 -rotate-22">
             <div className="relative aspect-9/19 w-44 sm:w-52 md:w-60 rounded-[2.5rem] bg-white shadow-[0_40px_100px_rgba(0,0,0,0.35)]">
               <div className="absolute inset-[6%] rounded-4xl overflow-hidden bg-yellow-400 flex items-center justify-center">
                 <div className="w-14 h-14 rounded-xl bg-white/90" />
